7cd57c01091ff362d1d750c7ecc67be53f999323
Ritik raja <110325525+Ritikraja07@users.noreply.github.com>
2024-07-24 05:53:58 +0000

    chat-app

---
+++ Chat-app-React-main/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Join from './components/Join';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Join/>
+    </div>
+  );
+}
+
+export default App;
